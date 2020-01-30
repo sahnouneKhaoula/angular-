f610db27f13f0f5a664af5b5eae3144cb755056b
khaoula sahnoune <sahnounekhaoula35@gmail.com>
2020-01-31 00:28:12 +0100

    update

--- src/app/components/exemple4/exemple4.component.ts
+++ src/app/components/exemple4/exemple4.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TrainingService } from 'src/app/services/training/training.service';
+import { TrainingService } from '../../services/training/training.service';
 import { Observable, interval, timer } from 'rxjs';
 import { map, tap, take, mergeMap, mergeAll, mergeMapTo, switchMap, concatMap, delay } from 'rxjs/operators';
 import { Training } from 'src/app/model/training';
